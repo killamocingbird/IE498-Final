caa49507d35e06b3cbbd25aca089e0f35fce2177
Justin <jlwang5@illinois.edu>
2020-05-07 23:01:55 -0500

    Add download script

--- temp.py
+++ temp.py
@@ -1,2 +1,10 @@
+# Downloads all data, only run once
+
 import dataset_coco as d
+# Downloads training data
+d.download_coco()
+# Downloads validation data
 d.download_coco(dataType=d.DataType.Val)
+# Downloads annotations
+d.download_annotations()
+
